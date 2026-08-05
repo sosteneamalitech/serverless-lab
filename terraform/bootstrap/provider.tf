be65d10f90ff923919bd97792ac7fc00e7797e3e
Sostene Munezero Bagira <sostene.munezero@amalitech.com>
2026-08-05 22:31:59 +0200

    chore: add the backend state

---
+++ terraform/bootstrap/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = "sostene.amalitech"
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      ManagedBy   = "terraform"
+      Environment = "sandbox"
+      Purpose     = "tfstate-bootstrap"
+    }
+  }
+}
